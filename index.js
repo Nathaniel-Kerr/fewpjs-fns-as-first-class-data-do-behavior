23f8292f276c84e91f9fb531830b16fbdae7b676
Nathaniel <NathanielKerr10@gmail.com>
2021-06-30 16:05:51 -0700

    commit tree 0a5f254d8fb00e3908eb76fa907af8e4392ea889

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(timeString) {
+  let time = parseInt(timeString);
+  if (time < 12) return "Good Morning"
+  else if (time > 17) return "Good Evening"
+  else return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  let t = document.getElementById('greeting')
+  t.innerHTML = msg
+}
